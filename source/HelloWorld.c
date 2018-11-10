1a6b0c7e5300093b21648c2f67f724d7bb6f5cbe
yhm <hyan666666@gmail.com>
2018-11-10 19:11:59 +0800

    Create a Hello World repository.

---
+++ source/HelloWorld.c
@@ -0,0 +1,19 @@
+/*
+ * HelloWorld.c
+ *
+ *  Created on: 2018Äê11ÔÂ10ÈÕ
+ *      Author: yhm
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World ! \n");
+	printf("Press any key to continue...");
+	getchar();
+
+	return 0;
+}
+
+
